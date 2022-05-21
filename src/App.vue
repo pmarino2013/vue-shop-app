4277b3bc4dbbd86e6a7abcea0d61bd3b67878e83
Pablo Marino <pmarino2013@gmail.com>
2022-05-21 18:44:25 -0300

    Primer commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+import ProductCard from "./components/ProductCard.vue";
+</script>
+
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <h1>Vue Shop</h1>
+        <hr />
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <ProductCard />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /* text-align: center; */
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/ProductCard.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="col-12 col-md-6">
+    <div class="card">
+      <div class="card-body">
+        <img
+          src="https://cdn-images.threadless.com/threadless-media/artist_shops/shops/vue/products/195796/shirt-1490420513-abd7b25b157f3450ce8c2a09ee51c36e.png?v=3&d=eyJvbmx5X21ldGEiOiBmYWxzZSwgImZvcmNlIjogZmFsc2UsICJvcHMiOiBbWyJ0cmltc2NyaXB0IiwgWzEyNzAuNTcxNDI4NTcxNDI4NCwgMTUwOC44NTcxNDI4NTcxNDI3XSwge31dLCBbImVuY29kZSIsIFsiLnBuZyJdLCB7ImRwaSI6IDMwMH1dLCBbInJlc2l6ZSIsIFs2NDRdLCB7fV0sIFsib3ZlcmxheSIsIFsidGhyZWFkbGVzcy1tZWRpYS9hcnRpc3Rfc2hvcHMvb3ZlcmxheXMvM2RmMjAzNDk1NGU0YzdlMTFhYmQ2NTc5NTRjZDQzZTcvZnJvbnQtMTU3MTMyODM4Ni1lZTg1NTlhN2Y1OGUxZDA0ZTU0M2MwNGJhMTVjM2ZjNS5wbmciXSwgeyJ5IjogNDgwLCAieCI6IDY4MSwgImJhY2tncm91bmQiOiAiZmZmZmZmIn1dLCBbInJlc2l6ZSIsIFtdLCB7IndpZHRoIjogMzUwLCAic3R5bGUiOiAiQ1JPUCIsICJoZWlnaHQiOiAzNTB9XSwgWyJlbmNvZGUiLCBbImpwZyIsIDg1XSwge31dXX0="
+          alt="camiseta"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.card img {
+  width: 100%;
+}
+</style>
